b31f2d0692096779d5b35d43455b48d806b0ea5c
Petr Antonov <antpeter@gmail.com>
2017-07-28 18:01:23 +0300

    Tuning models view in admin panel.

--- about_app/models.py
+++ about_app/models.py
@@ -6,8 +6,14 @@ class Work(models.Model):
     place = models.CharField(max_length=32)
     position = models.CharField(max_length=32)
 
+    def __str__(self):
+        return f'{self.id}. {self.place}'
+
 
 class Education(models.Model):
     years = models.CharField(max_length=10)
     place = models.CharField(max_length=32)
     position = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f'{self.id}. {self.place}'
