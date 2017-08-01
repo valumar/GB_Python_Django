0934a0c2c98e9b3e6e05dec0f18fff7e9d2681a0
Petr Antonov <antpeter@gmail.com>
2017-08-01 17:32:55 +0300

    Creating new model Organization and perfom `makemigrations` and `migrate`.

--- about_app/admin.py
+++ about_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Work, Education
+from .models import Work, Education, Organization
 
+admin.site.register(Organization)
 admin.site.register(Work)
 admin.site.register(Education)
--- about_app/models.py
+++ about_app/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 
+class Organization(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=48)
+    region = models.CharField(verbose_name='Регион', max_length=32)
+    site = models.CharField(verbose_name='Сайт', max_length=64, blank=True)
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
 class Work(models.Model):
+    organization = models.ForeignKey(Organization, verbose_name='Организация', null=False)
     years = models.CharField(max_length=10)
-    place = models.CharField(max_length=32)
+    # place = models.CharField(max_length=32)
     position = models.CharField(max_length=32)
 
     def __str__(self):
-        return f'{self.id}. {self.place}'
+        return f'{self.id}. {self.organization.name}'
 
 
 class Education(models.Model):
